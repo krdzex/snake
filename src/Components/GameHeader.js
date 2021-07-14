7f34e4950bb1a59e95f4ea5bf532b00d1a6ce3c0
Krsto Kostic <kostic.krsto@gmail.com>
2021-07-14 20:35:06 +0200

    adding gifs

--- src/Components/GameHeader.js
+++ src/Components/GameHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import coinGif from "../Gifs/coinSpining.gif"
+import snake from "../Gifs/snake.gif"
 const GameHeader = ({ score, gameOver, playAgain }) => {
     let getHighestScore = localStorage.getItem("highScore")
     let highestScore = getHighestScore === null ? 0 : getHighestScore;
@@ -7,10 +8,12 @@ const GameHeader = ({ score, gameOver, playAgain }) => {
     return (
         <div className="gameHeader">
             <div className="score">Score: {score}
+
+                <img style={{ marginLeft: 20 }} src={coinGif} width="30" height="30px" alt="coin Spining" />
             </div>
-            {gameOver && <div className="button" onClick={playAgain}>
+            {gameOver ? <div className="button" onClick={playAgain}>
                 Play Again
-            </div>}
+            </div> : <div className="snake"><img style={{ marginLeft: 20 }} src={snake} width="80px" height="50px" alt="snake" /></div>}
             <div>Highest: {highestScore}</div>
         </div>
     );
